4bcb118da605414e240d7295610a2298529d05a6
Edem Morny <emorny@gmail.com>
2018-10-09 18:56:48 +0000

    added readme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
+declare var vibes: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  username: string='joe@example.com';
+  password: string="password";
+  
+  constructor(public navCtrl: NavController,public toastCtrl: ToastController) {
+    vibes.configure();
   }
 
+  login(){
+    vibes.registerDevice();
+    let toast = this.toastCtrl.create({
+      message: 'User logged in',
+      duration: 3000
+    });
+    toast.present();
+  }
 }
